fcdb97947e6d59f2672228118cfab6a3ceafde59
Rubin Simons <me@rubin55.org>
2023-12-03 19:57:31 +0100

    Verified working of all dependencies with scala-native.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,18 @@
 
 import scopt.OParser
-
+import os.size
 import scalanative.unsafe.*
+
+import coulomb.*
+import coulomb.syntax.*
+import cats.syntax.either._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import io.circe.scalayaml._
+import io.circe.scalayaml.syntax._
+import scala.language.implicitConversions
+import coulomb.units.si.*
 
 @extern
 private object other {
@@ -30,10 +41,11 @@ case class Config(
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // Verify that c function calls work.
+    // Test c function calls.
     val exit = system(c"ls -l > test.txt")
     println(s"Hello, native Scala world, I got ${add3(3)} from a native C function.. and exit from some command was: $exit")
 
+    // Test scopt.
     val builder = OParser.builder[Config]
     val parser1 = {
       import builder._
@@ -48,12 +60,37 @@ object Main {
       )
     }
 
-    // OParser.parse returns Option[Config]
     OParser.parse(parser1, args, Config()) match {
       case Some(config) =>
       // do something
       case _ =>
       // arguments are bad, error message will have been displayed
     }
+
+    // Test os-lib. Note github.com/com-lihaoyi/os-lib/issues/197
+    val temp = os.temp()
+    val size = os.size(temp)
+    println(s"Temp file $temp is $size")
+
+    // Test coulomb.
+    val a = 9.8.withUnit[Meter / (Second ^ 2)]
+    println(s"Coulomb says: ${a.show}")
+
+    // Test circe.
+    sealed trait Foo
+    case class Bar(xs: Vector[String]) extends Foo
+    case class Qux(i: Int, d: Option[Double]) extends Foo
+    val foo: Foo = Qux(13, Some(14.0))
+    val json = foo.asJson.noSpaces
+    println(json)
+
+    // Test circe-scala-yaml
+    val json2 = io.circe.jawn.parse("""{"foo":"bar"}""").valueOr(throw _)
+    println(json2.asYaml.spaces2)
+
+    // Test scala-xml
+    val book: scala.xml.Elem = <book id="b20234">Magic of scala-xml</book>
+    val id = book \@ "id"
+    println(s"Book $id text is: ${book.text}")
   }
 }
